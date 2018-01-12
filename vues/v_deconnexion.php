fd082b6494bb1567017ca4db4fbb4ad28e53d708
anthony <anthony.lodovici@gmail.com>
2018-01-12 16:39:25 +0100

    changement couleur déconnexion comptable

--- vues/v_deconnexion.php
+++ vues/v_deconnexion.php
@@ -14,10 +14,17 @@
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  */
 deconnecter();
-?>
+?><?php if ($_SESSION['id']!==comptable){?>
 <div class="alert alert-info" role="alert">
     <p>Vous avez bien été déconnecté ! <a href="index.php">Cliquez ici</a>
         pour revenir à la page de connexion.</p>
 </div>
 <?php
 header("Refresh: 3;URL=index.php");
+}else{?>
+    <div class="alert alert-warning" role="alert">
+    <p>Vous avez bien été déconnecté ! <a href="index.php">Cliquez ici</a>
+        pour revenir à la page de connexion.</p>
+</div>
+<?php
+}
